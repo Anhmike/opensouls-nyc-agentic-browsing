8ace7ca54693e4ae41c27b18a6138056147bb30a
Topper Bowers <topper@toppingdesign.com>
2024-05-16 00:04:03 +0200

    remove comments

--- src/soulSupport.ts
+++ src/soulSupport.ts
@@ -36,10 +36,6 @@ export class SoulBrowser {
       await this.mustLoader().pageUp()
       return this.toolUseReturn()
     })
-
-    // this.soul.on("visit", this.onVisit.bind(this))
-    // this.soul.on("scrollDown", this.onScrollDown.bind(this))
-    // this.soul.on("scrollUp", this.onScrollUp.bind(this))
   }
 
   private async toolUseReturn() {
@@ -67,56 +63,6 @@ export class SoulBrowser {
     await this.soul.disconnect()
     this.loader?.close()
   }
-
-  // async onVisit(evt: ActionEvent) {
-  //   console.log("on visit event", await evt.content())
-  //   await this.mustLoader().visit(await evt.content())
-  //   const html = await this.mustLoader().captureVisibleHtmlTree()
-  //   const markdown = htmlToMarkdown(html)
-
-  //   const screenshot = await this.mustLoader().screenshot()
-
-  //   this.soul.dispatch({
-  //     action: "visited",
-  //     content: `Readerman visited ${this.mustLoader().url}`,
-  //     _metadata: {
-  //       content: markdown,
-  //       screenshot: `data:image/png;base64,${screenshot}`
-  //     }
-  //   })
-  // }
-
-  // async onScrollDown(_evt: ActionEvent) {
-  //   await this.mustLoader().pageDown()
-  //   const html = await this.mustLoader().captureVisibleHtmlTree()
-  //   const markdown = htmlToMarkdown(html)
-  //   const screenshot = await this.mustLoader().screenshot()
-
-  //   this.soul.dispatch({
-  //     action: "scrolledDown",
-  //     content: `Readerman scrolled down`,
-  //     _metadata: {
-  //       screenshot: `data:image/png;base64,${screenshot}`,
-  //       content: markdown,
-  //     }
-  //   })
-  // }
-
-  // async onScrollUp(_evt: ActionEvent) {
-  //   await this.mustLoader().pageUp()
-  //   const html = await this.mustLoader().captureVisibleHtmlTree()
-  //   const markdown = htmlToMarkdown(html)
-  //   const screenshot = await this.mustLoader().screenshot()
-
-  //   this.soul.dispatch({
-  //     action: "scrolledUp",
-  //     content: `Readerman scrolled up`,
-  //     _metadata: {
-  //       screenshot: `data:image/png;base64,${screenshot}`,
-  //       content: markdown,
-  //     }
-  //   })
-  // }
 
   private mustLoader() {
     if (!this.loader) {
